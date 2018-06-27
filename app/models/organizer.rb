0fc419d61eadd150a86306318ca60d533160e2cd
Adjoa <adjoa.darien@gmail.com>
2018-06-27 17:52:56 +0000

    organizer has_many events

--- app/models/organizer.rb
+++ app/models/organizer.rb
@@ -1,4 +1,6 @@
 class Organizer < ApplicationRecord
   has_one :address, as: :addressable, dependent: :destroy
+  has_many :events, dependent: :destroy
+  
   accepts_nested_attributes_for :address
 end
