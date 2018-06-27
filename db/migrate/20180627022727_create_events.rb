66aa40ad2efa94681a9617e0f3f27567236250d8
Adjoa <adjoa.darien@gmail.com>
2018-06-27 02:37:05 +0000

    init events migration

---
+++ db/migrate/20180627022727_create_events.rb
@@ -0,0 +1,16 @@
+class CreateEvents < ActiveRecord::Migration[5.2]
+  def change
+    create_table :events do |t|
+      t.string :name
+      t.date :date
+      t.time :time
+      t.text :description
+      t.string :contact_email
+      t.string :contact_phone
+      t.string :website
+      t.belongs_to :organizer, foreign_key: true
+      
+      t.timestamps
+    end
+  end
+end
