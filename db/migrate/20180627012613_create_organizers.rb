88b420a75af69c3e307007a019f57995057ef8a2
Adjoa <adjoa.darien@gmail.com>
2018-06-27 02:36:32 +0000

    init organizers migration

---
+++ db/migrate/20180627012613_create_organizers.rb
@@ -0,0 +1,11 @@
+class CreateOrganizers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :organizers do |t|
+      t.string :name
+      t.string :email
+      t.string :phone
+
+      t.timestamps
+    end
+  end
+end
