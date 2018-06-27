533e828c485bb357db8f855a2573a4dbb3ca09d0
Adjoa <adjoa.darien@gmail.com>
2018-06-27 18:55:37 +0000

    added event validations

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,6 @@ class Event < ApplicationRecord
   belongs_to :organizer
   has_one :address, as: :addressable, dependent: :destroy
   accepts_nested_attributes_for :address
+  
+  validates :name, :date, :time, :address, presence: true
 end
