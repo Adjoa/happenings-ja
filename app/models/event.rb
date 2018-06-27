48833953e09dbc10211c3c1f87581baccd050d65
Adjoa <adjoa.darien@gmail.com>
2018-06-27 19:35:54 +0000

    updated migration and model to reflect start_time and end_time added to event

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,5 +3,5 @@ class Event < ApplicationRecord
   has_one :address, as: :addressable, dependent: :destroy
   accepts_nested_attributes_for :address
   
-  validates :name, :date, :time, :address, presence: true
+  validates :name, :date, :start_time, :address, presence: true
 end
--- db/migrate/20180627022727_create_events.rb
+++ db/migrate/20180627022727_create_events.rb
@@ -3,7 +3,8 @@ class CreateEvents < ActiveRecord::Migration[5.2]
     create_table :events do |t|
       t.string :name
       t.date :date
-      t.time :time
+      t.time :start_time
+      t.time :end_time
       t.text :description
       t.string :contact_email
       t.string :contact_phone
