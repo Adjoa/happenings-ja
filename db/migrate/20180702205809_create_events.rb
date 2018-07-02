936853c89b8be11daa9c973426807bd945207855
Adjoa <adjoa.darien@gmail.com>
2018-07-02 21:34:51 +0000

    modified events migration to accept time in datetime format, date attribute removed

--- db/migrate/20180702205809_create_events.rb
+++ db/migrate/20180702205809_create_events.rb
@@ -2,16 +2,13 @@ class CreateEvents < ActiveRecord::Migration[5.2]
   def change
     create_table :events do |t|
       t.string :name
-      t.date :date
-      t.time :start_time
-      t.time :end_time
+      t.datetime :start_time
+      t.datetime :end_time
       t.text :description
       t.string :contact_email
       t.string :contact_phone
       t.string :website
       t.belongs_to :organizer, foreign_key: true
-      
-      t.timestamps
     end
   end
 end
