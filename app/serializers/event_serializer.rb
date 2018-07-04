6fa13e17bcb2ea75f5d5c012e543b2acbc051352
Adjoa <adjoa.darien@gmail.com>
2018-07-04 14:51:56 +0000

    event serializer has attributes, has_one address, belongs_to organizer

--- app/serializers/event_serializer.rb
+++ app/serializers/event_serializer.rb
@@ -1,3 +1,5 @@
 class EventSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :name, :start_time, :end_time, :description, :contact_email, :contact_phone, :website
+  has_one :address
+  belongs_to :organizer
 end
