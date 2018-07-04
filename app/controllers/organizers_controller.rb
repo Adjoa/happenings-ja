a2284f22af45b1d27b31ad906736085c49c53470
Adjoa <adjoa.darien@gmail.com>
2018-07-04 15:59:10 +0000

    added #organizer_params whitelist

--- app/controllers/organizers_controller.rb
+++ app/controllers/organizers_controller.rb
@@ -4,5 +4,19 @@ class OrganizersController < ApplicationController
     @organizers = Organizer.all
     render json: @organizers, status: 200
   end
+  
+  private
+  
+  def organizer_params
+    params.require(:organizer).permit(:name, :email, :phone, 
+      addresses_attributes: [
+        :line1,
+        :line2,
+        :city,
+        :parish,
+        :country
+      ]
+    )
+  end
 
 end
