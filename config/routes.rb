4084ea8dd9752024e550722421ea6b6e50a16202
Adjoa <adjoa.darien@gmail.com>
2018-07-04 16:05:47 +0000

    in routes.rb, removed new and edit for organizers and events

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :events, only: [:index, :show]
   
-  resources :organizers do
-    resources :events, except: [:index, :show]
+  resources :organizers, except: [:new, :edit] do
+    resources :events, except: [:index, :show, :new, :edit]
   end
 end
