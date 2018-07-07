3ff8dc02412330169f455e523613f953523bb54d
Adjoa <adjoa.darien@gmail.com>
2018-07-07 19:37:04 +0000

    in AuthorizeApiRequest, add private method http_auth_header to check for a token in the Authorization header

--- app/auth/authorize_api_request.rb
+++ app/auth/authorize_api_request.rb
@@ -25,7 +25,16 @@ class AuthorizeApiRequest
     )
   end
   
+  # decode authentication token
   def decoded_auth_token
     @decoded_auth_token ||= JsonWebToken.decode(http_auth_header)
+  end
+  
+  # check for token in `Authorization` header
+  def http_auth_header
+    if headers['Authorization'].present?
+      return headers['Authorization'].split(' ').last
+    end
+      raise(ExceptionHandler::MissingToken, Message.missing_token)
   end
 end
