37a1684e5cf741566493fa43aa0c4a596b9fd776
Adjoa <adjoa.darien@gmail.com>
2018-07-07 20:06:31 +0000

    added #auth_params to AuthenticationController

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -5,4 +5,11 @@ class AuthenticationController < ApplicationController
       AuthenticateUser.new(auth_params[:email], auth_params[:password]).call
     json_response(auth_token: auth_token)
   end
+  
+  private
+
+  def auth_params
+    params.permit(:email, :password)
+  end
+  
 end
