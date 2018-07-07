d7b38c7aecdf1807e60ec4b09cefea67701d0a82
Adjoa <adjoa.darien@gmail.com>
2018-07-07 20:08:05 +0000

    added auth/login endpoint to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :events, only: [:index, :show]
   
+  post 'auth/login', to: 'authentication#authenticate'
+  
+  resources :events, only: [:index, :show]
   resources :organizers, except: [:new, :edit] do
     resources :events, except: [:index, :show, :new, :edit]
   end
