251ce3004682ff10312ab3437635cb582004d4c5
Adjoa <adjoa.darien@gmail.com>
2018-07-07 19:23:39 +0000

    added AuthorizeApiRequest#call --returns organizer object on valid request

--- app/auth/authorize_api_request.rb
+++ app/auth/authorize_api_request.rb
@@ -2,4 +2,9 @@ class AuthorizeApiRequest
   def initialize(headers = {})
     @headers = headers
   end
+  
+  # Return organizer object upon receipt of valid request
+  def call
+    { organizer: organizer }
+  end
 end
