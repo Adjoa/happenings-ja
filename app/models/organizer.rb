dd09d29085b4a38300e79cb00f0d22fa34876507
Adjoa <adjoa.darien@gmail.com>
2018-07-07 18:57:01 +0000

    organizer model has_secure_password

--- app/models/organizer.rb
+++ app/models/organizer.rb
@@ -2,11 +2,13 @@ class Organizer < ApplicationRecord
   has_one :address, as: :addressable, dependent: :destroy
   has_many :events, dependent: :destroy
   
-  accepts_nested_attributes_for :address
-  
   validates :name, :password_digest, presence: true
   validates :email, 
             :presence => true, 
             :uniqueness => true, 
             :format => { :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
+  
+  accepts_nested_attributes_for :address
+  
+  has_secure_password
 end
