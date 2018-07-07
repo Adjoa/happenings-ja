ab4ada00ceb6742fac40b26e9a795747d91c7833
Adjoa <adjoa.darien@gmail.com>
2018-07-07 19:34:19 +0000

    in AuthorizeApiRequest, add private organizer method to handle return of organizer object

--- app/auth/authorize_api_request.rb
+++ app/auth/authorize_api_request.rb
@@ -7,4 +7,21 @@ class AuthorizeApiRequest
   def call
     { organizer: organizer }
   end
+  
+  private
+
+  attr_reader :headers
+
+  def organizer
+    # check if organizer is in the database
+    # memoize organizer object
+    @organizer ||= Organizer.find(decoded_auth_token[:organizer_id]) if decoded_auth_token
+    # handle organizer not found
+  rescue ActiveRecord::RecordNotFound => e
+    # raise custom error
+    raise(
+      ExceptionHandler::InvalidToken,
+      ("#{Message.invalid_token} #{e.message}")
+    )
+  end
 end
