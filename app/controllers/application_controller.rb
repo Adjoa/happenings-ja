36cd161bcffb61ea852809c2f35c0d523b6ff539
Adjoa <adjoa.darien@gmail.com>
2018-07-08 23:29:45 +0000

    added before_action and current_user attr_reader to Application Controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::API
   include Response
   include ExceptionHandler
+  
+  # called before every action on controllers
+  before_action :authorize_request
+  attr_reader :current_user
 end
