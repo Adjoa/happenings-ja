518034d2c6e92cf4903d755fd028d1d3731e3d91
Adjoa <adjoa.darien@gmail.com>
2018-07-09 01:22:06 +0000

    moved organizers and events controller into module v1 namespace

--- app/controllers/v1/events_controller.rb
+++ app/controllers/v1/events_controller.rb
@@ -1,58 +1,59 @@
-class EventsController < ApplicationController
-  before_action :set_organizer_event, only: [:update, :destroy]
-  before_action :set_event, only: [:show]
-  
-  def index
-    @events = Event.all ||= current_user.events if current_user
-    json_response(@events)
+module V1
+  class EventsController < ApplicationController
+    before_action :set_organizer_event, only: [:update, :destroy]
+    before_action :set_event, only: [:show]
+    
+    def index
+      @events = Event.all ||= current_user.events if current_user
+      json_response(@events)
+    end
+    
+    def create
+      @event = current_user.events.create!(event_params)
+      json_response(@event, :created)
+    end
+    
+    def show
+      json_response(@event)
+    end
+    
+    def update
+      @event.update
+      head :no_content
+    end
+    
+    def destroy
+      @event.destroy
+      head :no_content
+    end
+    
+    private
+    
+    def event_params
+      params.require(:event).permit(
+        :name, 
+        :start_time, 
+        :end_time, 
+        :description, 
+        :contact_email, 
+        :contact_phone, 
+        :website, 
+        address_attributes: [
+          :line1,
+          :line2,
+          :city,
+          :parish,
+          :country
+        ]
+      )
+    end
+    
+    def set_organizer_event
+      @event = current_user.events.find_by!(id: params[:id]) if current_user
+    end
+    
+    def set_event
+      @event = Event.find(params[:id])
+    end
   end
-  
-  def create
-    @event = current_user.events.create!(event_params)
-    json_response(@event, :created)
-  end
-  
-  def show
-    json_response(@event)
-  end
-  
-  def update
-    @event.update
-    head :no_content
-  end
-  
-  def destroy
-    @event.destroy
-    head :no_content
-  end
-  
-  private
-  
-  def event_params
-    params.require(:event).permit(
-      :name, 
-      :start_time, 
-      :end_time, 
-      :description, 
-      :contact_email, 
-      :contact_phone, 
-      :website, 
-      address_attributes: [
-        :line1,
-        :line2,
-        :city,
-        :parish,
-        :country
-      ]
-    )
-  end
-  
-  def set_organizer_event
-    @event = current_user.events.find_by!(id: params[:id]) if current_user
-  end
-  
-  def set_event
-    @event = Event.find(params[:id])
-  end
-  
 end
--- app/controllers/v1/organizers_controller.rb
+++ app/controllers/v1/organizers_controller.rb
@@ -1,54 +1,55 @@
-class OrganizersController < ApplicationController
-  skip_before_action :authorize_request, only: :create
-  before_action :set_organizer, only: [:show, :update, :destroy]
-  
-  def index
-    @organizers = Organizer.all
-    json_response(@organizers)
+module V1
+  class OrganizersController < ApplicationController
+    skip_before_action :authorize_request, only: :create
+    before_action :set_organizer, only: [:show, :update, :destroy]
+    
+    def index
+      @organizers = Organizer.all
+      json_response(@organizers)
+    end
+    
+    def create
+      @organizer = Organizer.create!(organizer_params)
+      auth_token = AuthenticateUser.new(organizer.email, organizer.password).call
+      response = { message: Message.account_created, auth_token: auth_token }
+      json_response(response, :created)
+    end
+    
+    def show
+      json_response(@organizer)
+    end
+    
+    def update
+      @organizer.update(organizer_params)
+      head :no_content
+    end
+    
+    def destroy
+      @organizer.destroy
+      head :no_content
+    end
+    
+    private
+    
+    def organizer_params
+      params.require(:organizer).permit(
+        :name, 
+        :email, 
+        :password, 
+        :password_confirmation,
+        :phone, 
+        address_attributes: [
+          :line1,
+          :line2,
+          :city,
+          :parish,
+          :country
+        ]
+      )
+    end
+    
+    def set_organizer
+      @organizer = Organizer.find(params[:id])
+    end
   end
-  
-  def create
-    @organizer = Organizer.create!(organizer_params)
-    auth_token = AuthenticateUser.new(organizer.email, organizer.password).call
-    response = { message: Message.account_created, auth_token: auth_token }
-    json_response(response, :created)
-  end
-  
-  def show
-    json_response(@organizer)
-  end
-  
-  def update
-    @organizer.update(organizer_params)
-    head :no_content
-  end
-  
-  def destroy
-    @organizer.destroy
-    head :no_content
-  end
-  
-  private
-  
-  def organizer_params
-    params.require(:organizer).permit(
-      :name, 
-      :email, 
-      :password, 
-      :password_confirmation,
-      :phone, 
-      address_attributes: [
-        :line1,
-        :line2,
-        :city,
-        :parish,
-        :country
-      ]
-    )
-  end
-  
-  def set_organizer
-    @organizer = Organizer.find(params[:id])
-  end
-
 end
