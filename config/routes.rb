e497383cfda02261d523c8124fe4174c3475b29a
Adjoa <adjoa.darien@gmail.com>
2018-07-09 01:16:25 +0000

    minor format change

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
-  post 'auth/login', to: 'authentication#authenticate'
-  post 'signup', to: 'organizers#create'
-  
   # namespace the controllers without affecting the URI
   scope module: :v1, constraints: ApiVersion.new('v1', true) do
     resources :events, only: [:index, :show]
@@ -12,5 +9,7 @@ Rails.application.routes.draw do
     end
   end
   
-
+  post 'auth/login', to: 'authentication#authenticate'
+  post 'signup', to: 'organizers#create'
+  
 end
