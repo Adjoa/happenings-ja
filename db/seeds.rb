555d69ed038aa4dce4f144287af36b8702c328b7
Adjoa <adjoa.darien@gmail.com>
2018-07-09 00:15:25 +0000

    updated seeds.rb with password for Organizer

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@ require 'date'
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Organizer.create(name:"The Mandeville Hotel", email:"themandevillehotel@email.com")
+Organizer.create(name:"The Mandeville Hotel", email:"themandevillehotel@email.com", password: "password", password_confirmation: "password")
 Organizer.all.last.create_address(line1:"The Mandeville Hotel", line2:"4 Hotel Street", city:"Mandeville", parish:"Manchester", country:"Jamaica")
 Organizer.all.last.events.create(name:"Open Mic Night", start_time: DateTime.parse('June 26 2018 07:00:00 PM'), end_time: DateTime.parse('June 26 2018 10:00:00 PM'), contact_email:"themandevillehotel@email.com")
 Organizer.all.last.events.all.last.create_address(line1:"The Mandeville Hotel", line2:"4 Hotel Street", city:"Mandeville", parish:"Manchester", country:"Jamaica")
