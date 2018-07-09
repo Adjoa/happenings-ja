77fd5ddfea93ae43475295b1887337869b3ff402
Adjoa <adjoa.darien@gmail.com>
2018-07-09 01:11:29 +0000

    added private ApiVersion#check_headers

--- app/lib/api_version.rb
+++ app/lib/api_version.rb
@@ -10,4 +10,13 @@ class ApiVersion
   def matches?(request)
     check_headers(request.headers) || default
   end
+  
+  private
+
+  def check_headers(headers)
+    # check version from Accept headers; expect custom media type `todos`
+    accept = headers[:accept]
+    accept && accept.include?("application/vnd.todos.#{version}+json")
+  end
+  
 end
