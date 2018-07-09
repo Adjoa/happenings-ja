e51f2c433b84877e737ffc091599baa68fb47e83
Adjoa <adjoa.darien@gmail.com>
2018-07-09 01:15:31 +0000

    add version 1 scope to routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,13 @@ Rails.application.routes.draw do
   post 'auth/login', to: 'authentication#authenticate'
   post 'signup', to: 'organizers#create'
   
-  resources :events, only: [:index, :show]
-  resources :organizers, except: [:new, :edit] do
-    resources :events, except: [:index, :show, :new, :edit]
+  # namespace the controllers without affecting the URI
+  scope module: :v1, constraints: ApiVersion.new('v1', true) do
+    resources :events, only: [:index, :show]
+    resources :organizers, except: [:new, :edit] do
+      resources :events, except: [:index, :show, :new, :edit]
+    end
   end
+  
+
 end
