dae99c16de313a306e5a117e0f5a1226802cb5ac
Adjoa <adjoa.darien@gmail.com>
2018-07-16 22:52:29 +0000

    Navbar-> NavBar, import NavLink and link to App

---
+++ client/src/components/NavBar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const NavBar = () => {
+  return (
+    <div>
+      <NavLink to="/search">Find an event</NavLink>&nbsp;
+      { /* <NavLink to="/organize">Submit an event</NavLink> */ }
+    </div>
+  );
+};
+
+export default NavBar;
