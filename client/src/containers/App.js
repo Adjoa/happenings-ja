6f4d75ba68130909ee68990ae8daa33f2c94c065
Adjoa <adjoa.darien@gmail.com>
2018-07-16 22:55:38 +0000

    in App.js, removed Welcome, added NavBar and an event list filler paragraph

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import Header from '../components/Header';
+import NavBar from '../components/NavBar';
 import Footer from '../components/Footer';
-import Welcome from '../components/Welcome';
 
 class App extends Component {
   render() {
     return (
       <div>
         <Header/>
-        <Welcome/>
+        <NavBar />
+        <p> A list of events</p>
         <Footer/>
       </div>
     );
