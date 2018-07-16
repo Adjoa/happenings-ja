1619ad853a2e3eb3a1069b5fd385ead9125c5ec1
Adjoa <adjoa.darien@gmail.com>
2018-07-16 22:46:13 +0000

    index.js renders Router component

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './css/index.css';
-import App from './containers/App';
+import { render } from 'react-dom';
+import Router from './components/Router';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+render(<Router />, document.getElementById('root'));
+
 registerServiceWorker();
