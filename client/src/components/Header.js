47c45e359adcab2643ce9506f32e10a7bd9a9b2a
Adjoa <adjoa.darien@gmail.com>
2018-07-16 22:50:23 +0000

    in Header component, import NavLink and link to Welcome.js

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const Header = () => (
   <header>
-    <h1>Happenings JA</h1>
+    <NavLink to="/"><h1>Happenings JA</h1></NavLink>
   </header>
   );
   
