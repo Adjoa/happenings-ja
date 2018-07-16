37d1cceb313b725e87d12dc8a909930d8c4164e0
Adjoa <adjoa.darien@gmail.com>
2018-07-16 00:28:13 +0000

    in events_controller, removed refs to current_user

--- app/controllers/v1/events_controller.rb
+++ app/controllers/v1/events_controller.rb
@@ -1,15 +1,15 @@
 module V1
   class EventsController < ApplicationController
-    before_action :set_organizer_event, only: [:update, :destroy]
-    before_action :set_event, only: [:show]
+    # before_action :set_organizer_event, only: [:update, :destroy]
+    # before_action :set_event, only: [:show]
     
     def index
-      @events = Event.all ||= current_user.events if current_user
+      @events = Event.all
       json_response(@events)
     end
     
     def create
-      @event = current_user.events.create!(event_params)
+      @event = Event.create!(event_params)
       json_response(@event, :created)
     end
     
@@ -32,10 +32,6 @@ module V1
     def event_params
       params.require(:event).permit(:name, :start_time, :end_time, :description, 
         address_attributes: [:line1, :line2, :city, :parish, :country])
-    end
-    
-    def set_organizer_event
-      @event = current_user.events.find_by!(id: params[:id]) if current_user
     end
     
     def set_event
