c8014bd97992d9264c4cd8f420701192f4bf6586
Adjoa <adjoa.darien@gmail.com>
2018-07-16 19:43:56 +0000

    App.js will find components in the components/ folder

---
+++ client/src/components/Welcome.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Welcome = () => (
+  <section>
+    <h1>Find your next adventure.</h1>
+    <h1><a href="#">Start</a></h1>
+  </section>
+  
+  );
+
+export default Welcome;
