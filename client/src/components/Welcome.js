cb5dbfa409852f0da9613677e0e73c9a64fc8ecc
Adjoa <adjoa.darien@gmail.com>
2018-07-17 17:09:36 +0000

    in Welcome, updated 'Start' link /search -> /events

--- client/src/components/Welcome.js
+++ client/src/components/Welcome.js
@@ -4,7 +4,7 @@ import { NavLink } from 'react-router-dom';
 const Welcome = () => (
   <section>
     <h1>Find your next adventure.</h1>
-    <h1><NavLink to="/search">Start</NavLink></h1>
+    <h1><NavLink to="/events">Start</NavLink></h1>
   </section>
   
   );
