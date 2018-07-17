6e092256d4bd21e44a06cd6aee9651a2bdb75bb8
Adjoa <adjoa.darien@gmail.com>
2018-07-17 17:47:46 +0000

    in src/index.js, initialized store using redux and wrapper Router in a Provider

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Provider } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers'
 import Router from './components/Router';
 import registerServiceWorker from './registerServiceWorker';
 
-render(<Router />, document.getElementById('root'));
+const store = createStore(rootReducer, applyMiddleware(thunk));
+
+render(
+  <Provider store={ store } >
+  <Router />
+  </Provider>, document.getElementById('root')
+  );
 
 registerServiceWorker();
