208fda04410f0734ffd2ac01b478b47b43899904
Adjoa <adjoa.darien@gmail.com>
2018-07-17 17:39:08 +0000

    init events_reducer.js

---
+++ client/src/reducers/events_reducer.js
@@ -0,0 +1,11 @@
+export default function eventsReducer(state = { loading: false, eventItems: [] }, action) {
+  switch (action.type) {
+    case 'LOADING_EVENTS':
+      return Object.assign({}, state, {loading: true})
+    case 'GET_EVENTS':
+      return { loading: false, eventItems: action.payload }
+ 
+    default:
+      return state;
+  }
+};
