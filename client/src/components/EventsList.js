9a91b0f3b99e40f7ac88560def2bbc4308b25436
Adjoa <adjoa.darien@gmail.com>
2018-07-17 18:06:54 +0000

    updated EventsList to map events to EventsListItem components

--- client/src/components/EventsList.js
+++ client/src/components/EventsList.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import EventsListItem from './EventsListItem';
 
-const EventsList = () => (
-  <div>This is an events list</div>
+const EventsList = (props) => {
+  const eventsListItems = props.events.map(listItem => 
+    <li key={listItem.id}>
+      <EventsListItem event={listItem} />
+    </li>
   );
+  
+  return(
+    <div>
+      <div>
+        <ul>
+          {eventsListItems}
+        </ul>
+      </div>
+    </div>
+  );
+}
   
 export default EventsList;
