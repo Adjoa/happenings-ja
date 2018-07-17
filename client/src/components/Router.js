4d488a4823b64b53958fa8c2af0d5de73c141650
Adjoa <adjoa.darien@gmail.com>
2018-07-17 00:41:46 +0000

    Router now handles display of Header, NavBar, and Footer and routes /events to EventsPage

--- client/src/components/Router.js
+++ client/src/components/Router.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import Header from '../components/Header';
+import NavBar from '../components/NavBar';
+import Footer from '../components/Footer';
+
 import Welcome from '../components/Welcome';
-import App from '../containers/App';
+import EventsPage from '../containers/EventsPage';
 import NotFound from '../components/NotFound';
 
 const Router = () => (  
     <BrowserRouter>
-    <Switch>
-      <Route exact path="/" component={Welcome} />
-      <Route exact path="/search" component={App} />
-      <Route component={NotFound} />
-    </Switch>
+    <div>
+      <Header/>
+      <NavBar/>
+      <Switch>
+        <Route exact path="/" component={Welcome} />
+        <Route exact path="/events" component={EventsPage} />
+        <Route component={NotFound} />
+      </Switch>  
+      <Footer/>
+    </div>
     </BrowserRouter>
   );
 
