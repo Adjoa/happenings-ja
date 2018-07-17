207f25ddc9e7726cfe4fb4efb7092f737decdcbf
Adjoa <adjoa.darien@gmail.com>
2018-07-17 00:40:31 +0000

    App.js -> EventsPage.js

---
+++ client/src/containers/EventsPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class EventsPage extends React.Component {
+  render() {
+    return (
+      <div>
+        <p> A list of events</p>
+      </div>
+    );
+  }
+}
+
+export default EventsPage;
