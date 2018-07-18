10e2497063a27381b62b0c8eb56f5927f71197eb
Adjoa <adjoa.darien@gmail.com>
2018-07-18 16:05:38 +0000

    in EventsListItem, added See More link to EventShow Page

--- client/src/components/EventsListItem.js
+++ client/src/components/EventsListItem.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { formatDescription } from '../helpers';
 
 const EventsListItem = ({ event }) => {
@@ -9,6 +10,7 @@ const EventsListItem = ({ event }) => {
       <span>{ event.start_time }</span>
       <h2>{ event.name }</h2>
       <p>{ descriptionAvailable ? formatDescription(event.description) : "No description provided." }</p>
+      <Link to={`/events/${event.id}`}>See More</Link>
     </div>
   );
 };
