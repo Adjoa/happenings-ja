fb7e37aea2126c02228ba889529941d194f94b4f
Adjoa <adjoa.darien@gmail.com>
2018-07-18 23:58:28 +0000

    added EventForm fields

--- client/src/components/EventForm.js
+++ client/src/components/EventForm.js
@@ -6,23 +6,26 @@ class EventForm extends React.Component {
       <div>
         <form>
           <h2>Organizer Info</h2>
-          <label>Name</label>
-          <input type="text" />
-          <label>Email</label>
-          <input type="email"></input>
-          <label>Phone</label>
-          <input type="tel"></input>
-          
+            <label>Name</label>
+            <input type="text" />
+            <label>Email</label>
+            <input type="email"></input>
+            <label>Phone</label>
+            <input type="tel"></input>
           <h2>Event Info</h2>
-          <label>Name</label>
-          <input type="text"></input>
-          <label>Time</label>
-          <input type="datetime-local"></input>
-          <label>"Address"</label>
-          <input></input>
-          <input></input>
-          <input></input>
-          <input></input>
+            <label>Name</label>
+            <input type="text"></input>
+            <label>Time</label>
+            <input type="datetime-local"></input>
+            <h3>Address</h3>
+              <label>Line 1</label>
+              <input type="text"></input>
+              <label>Line 2</label>
+              <input type="text"></input>
+              <label>City</label>
+              <input type="text"></input>
+              <label>Parish</label>
+              <input type="text"></input>
           <input type="submit" />
         </form>
       </div>
