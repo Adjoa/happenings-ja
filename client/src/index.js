063c589fdcfbdc57f30bf51955e359a4c10ef00d
Adjoa <adjoa.darien@gmail.com>
2018-07-18 01:11:50 +0000

    import redux compose to solve redux error:  the previous state received by the reducer has unexpected type of “Function

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { Provider } from 'redux';
-import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import { rootReducer } from './reducers'
 import Router from './components/Router';
 import registerServiceWorker from './registerServiceWorker';
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const middleware = applyMiddleware(thunk);
+const store = createStore(
+    rootReducer,
+    compose(middleware, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+  );
 
 render(
   <Provider store={ store } >
