b4196e289c8f7095eb624c4645cd4d602b286635
Adjoa <adjoa.darien@gmail.com>
2018-07-18 12:39:16 +0000

    events successfully retrieved from api! synced package.json, eventActions, and events_reducer

--- client/src/actions/eventActions.js
+++ client/src/actions/eventActions.js
@@ -3,8 +3,8 @@ import fetch from 'isomorphic-fetch'
 export function getEvents() {
   return (dispatch) => {
     dispatch({ type: 'LOADING_EVENTS' });
-    return fetch('events')
+    return fetch('/events')
       .then(response => response.json())
-      .then(events => dispatch({ type: 'GET_EVENTS', payload: events }));
+      .then(events => dispatch({ type: 'GET_EVENTS', events }));
   };
 }
--- client/src/reducers/events_reducer.js
+++ client/src/reducers/events_reducer.js
@@ -1,9 +1,10 @@
-export default function eventsReducer(state = { loading: false, eventItems: {} }, action) {
+export default function eventsReducer(state = { loading: false, eventItems: [] }, action) {
   switch (action.type) {
     case 'LOADING_EVENTS':
       return Object.assign({}, state, {loading: true})
     case 'GET_EVENTS':
-      return { loading: false, eventItems: action.payload }
+      console.log("Getting events...")
+      return { loading: false, eventItems: action.events }
  
     default:
       return state;
