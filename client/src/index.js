cdca5f13557540d1c5460620996410279508513c
Adjoa <adjoa.darien@gmail.com>
2018-07-18 20:58:01 +0000

    moved store related items into store.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,20 +1,13 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, compose } from 'redux';
-import thunk from 'redux-thunk';
-import { rootReducer } from './reducers'
+
 import Router from './components/Router';
+import store from './store';
 import registerServiceWorker from './registerServiceWorker';
 
-const middleware = applyMiddleware(thunk);
-const store = createStore(
-    rootReducer,
-    compose(middleware, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
-  );
-
 render(
-  <Provider store={ store } >
+  <Provider store={store} >
   <Router />
   </Provider>, document.getElementById('root')
   );
