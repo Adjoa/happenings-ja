0124784c79175fed2753a6fd202249a2fe273881
Adjoa <adjoa.darien@gmail.com>
2018-07-18 14:47:11 +0000

    in events_reducer, state.eventItems -> state.events

--- client/src/reducers/events_reducer.js
+++ client/src/reducers/events_reducer.js
@@ -1,10 +1,10 @@
-export default function eventsReducer(state = { loading: false, eventItems: [] }, action) {
+export default function eventsReducer(state = { loading: false, events: [] }, action) {
   switch (action.type) {
     case 'LOADING_EVENTS':
       return Object.assign({}, state, {loading: true})
     case 'GET_EVENTS':
       console.log("Getting events...")
-      return { loading: false, eventItems: action.events }
+      return { loading: false, events: action.events }
  
     default:
       return state;
