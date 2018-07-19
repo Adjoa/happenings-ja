efa00527945dd630f18ae877151ac08d950250a2
Adjoa <adjoa.darien@gmail.com>
2018-07-19 04:47:38 +0000

    updated db/seeds to reflect removal of organizer model

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@ require 'date'
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Organizer.create(name:"The Mandeville Hotel", email:"themandevillehotel@email.com")
-Organizer.all.last.create_address(line1:"The Mandeville Hotel", line2:"4 Hotel Street", city:"Mandeville", parish:"Manchester", country:"Jamaica")
-Organizer.all.last.events.create(name:"Open Mic Night", start_time: DateTime.parse('June 26 2018 07:00:00 PM'), end_time: DateTime.parse('June 26 2018 10:00:00 PM'))
-Organizer.all.last.events.all.last.create_address(line1:"The Mandeville Hotel", line2:"4 Hotel Street", city:"Mandeville", parish:"Manchester", country:"Jamaica")
+
+
+Event.create(name:"Open Mic Night", start_time: DateTime.parse('June 26 2018 07:00:00 PM'), end_time: DateTime.parse('June 26 2018 10:00:00 PM'))
+Event.all.last.create_address(line1:"The Mandeville Hotel", line2:"4 Hotel Street", city:"Mandeville", parish:"Manchester", country:"Jamaica")
