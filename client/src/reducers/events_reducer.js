44805b01b3576e02d51b266f889d4635aa1ee68b
Adjoa <adjoa.darien@gmail.com>
2018-07-19 17:56:48 +0000

    ADD_EVENT reducer correctly updated state! It works

--- client/src/reducers/events_reducer.js
+++ client/src/reducers/events_reducer.js
@@ -3,14 +3,11 @@ export default function eventsReducer(state = { loading: false, events: [] }, ac
     case 'LOADING_EVENTS':
       return Object.assign({}, state, {loading: true})
     case 'GET_EVENTS':
-      console.log("Getting events...")
-      // return { ...state, events: action.events }
       return Object.assign({}, state, {loading: false, events: action.events})
     case 'ADDING_EVENT':
       return Object.assign({}, state, {loading: true})
     case 'ADD_EVENT':
-      console.log("Adding event...")
-      return Object.assign({}, state, {loading: false, events: action.event})
+      return Object.assign({}, state, {loading: false, events: state.events.concat(action.event)})
     default:
       return state;
   }
