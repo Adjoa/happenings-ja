e39002f6fcb9f0a2211e96323c7b60773aa2ff0d
Adjoa <adjoa.darien@gmail.com>
2018-07-19 05:39:04 +0000

    update migrations to accept event times as strings

--- db/migrate/20180702205809_create_events.rb
+++ db/migrate/20180702205809_create_events.rb
@@ -2,8 +2,8 @@ class CreateEvents < ActiveRecord::Migration[5.2]
   def change
     create_table :events do |t|
       t.string :name
-      t.datetime :start_time
-      t.datetime :end_time
+      t.string :start_time
+      t.string :end_time
       t.text :description
     end
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,5 @@ require 'date'
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-
-
-Event.create(name:"Open Mic Night", start_time: DateTime.parse('June 26 2018 07:00:00 PM'), end_time: DateTime.parse('June 26 2018 10:00:00 PM'))
+Event.create(name:"Open Mic Night", start_time: 'June 26 2018 at 7:00 PM', end_time: 'June 26 2018 at 10:00 PM')
 Event.all.last.create_address(line1:"The Mandeville Hotel", line2:"4 Hotel Street", city:"Mandeville", parish:"Manchester", country:"Jamaica")
