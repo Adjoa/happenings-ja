6823f2b284f61491a62a1cb30417f2738358f45a
Adjoa <adjoa.darien@gmail.com>
2018-07-19 05:44:38 +0000

    added reducers for ADDING_EVENT and ADD_EVENT

--- client/src/reducers/events_reducer.js
+++ client/src/reducers/events_reducer.js
@@ -6,7 +6,11 @@ export default function eventsReducer(state = { loading: false, events: [] }, ac
       console.log("Getting events...")
       // return { ...state, events: action.events }
       return Object.assign({}, state, {loading: false, events: action.events})
- 
+    case 'ADDING_EVENT':
+      return Object.assign({}, state, {loading: true})
+    case 'ADD_EVENT':
+      console.log("Adding event...")
+      return Object.assign({}, state, {loading: false, events: action.event})
     default:
       return state;
   }
