b3a696cdf9180b9e4e260670a115641d173dfc3c
Adjoa <adjoa.darien@gmail.com>
2018-07-19 14:48:01 +0000

    removed Link from EventsListItem.js

--- client/src/components/EventsListItem.js
+++ client/src/components/EventsListItem.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import { formatDescription } from '../helpers';
 
 const EventsListItem = ({ event }) => {
@@ -8,7 +7,7 @@ const EventsListItem = ({ event }) => {
   return(
     <div>
       <span>{ event.start_time }</span>
-      <Link key={event.id} to={`/events/${event.id}`}>{ event.name }</Link>
+      <h2>{ event.name }</h2>
       <p>{ descriptionAvailable ? formatDescription(event.description) : "No description provided." }</p>
     </div>
   );
