91bf1d437f01f3a3e1ea4145d2591188cb4a747c
Adjoa <adjoa.darien@gmail.com>
2018-07-19 13:54:02 +0000

    App.js routes to Welcome EventsPage and NotFound

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -8,9 +8,6 @@ import EventsPage from './EventsPage';
 import NotFound from './NotFound';
 import Footer from './Footer';
 
-import EventForm from '../containers/EventForm';
-import EventShow from '../containers/EventShow';
-
 class App extends React.Component {
   componentDidMount() {
     this.props.getEvents()
@@ -27,12 +24,9 @@ class App extends React.Component {
           <NavBar/>
           <Switch>
             <Route exact path="/" component={Welcome} />
-            <Route exact path="/events" component={EventsPage} />
-            <Route exact path="/events/new" component={EventForm} />
-            <Route path={`/events/:eventId`} component={EventShow} />
+            <Route path="/events" component={EventsPage} />
             <Route component={NotFound} />
           </Switch>
-          {/* <EventsPage events={ this.props.events.events } />*/}
           <Footer/>
         </div> : "Loading..."
       }
