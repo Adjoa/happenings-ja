fae76ca5a355f48c3939c491b6f0615db7c5c270
Adjoa <adjoa.darien@gmail.com>
2018-07-19 04:45:09 +0000

    in create_events migration, remove organizer_id

--- db/migrate/20180702205809_create_events.rb
+++ db/migrate/20180702205809_create_events.rb
@@ -5,10 +5,6 @@ class CreateEvents < ActiveRecord::Migration[5.2]
       t.datetime :start_time
       t.datetime :end_time
       t.text :description
-      t.string :contact_email
-      t.string :contact_phone
-      t.string :website
-      t.belongs_to :organizer, foreign_key: true
     end
   end
 end
