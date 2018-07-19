72751f8c1455712fbb989f2af5e4c3ed7cfb437d
Adjoa <adjoa.darien@gmail.com>
2018-07-19 13:09:54 +0000

    EventsPage is a component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import Header from '../components/Header';
 import NavBar from '../components/NavBar';
-import EventsPage from '../containers/EventsPage';
+import EventsPage from '../components/EventsPage';
 import Footer from '../components/Footer';
 
 class App extends React.Component {
