90026ef7282eaf0b0cc073c425f8d74c487b7c26
Adjoa <adjoa.darien@gmail.com>
2018-07-19 14:48:36 +0000

    EventsList adds Links to items

--- client/src/components/EventsList.js
+++ client/src/components/EventsList.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import EventsListItem from './EventsListItem';
 import EventShow from '../containers/EventShow';
 
 const EventsList = ({ events }) => {
   const renderEvents = events.map(event => 
-    <li key={event.id}>
+    <Link key={event.id} to={`/events/${event.id}`}>
       <EventsListItem event={event} />
-    </li>
+    </Link>
   );
   
   return(
