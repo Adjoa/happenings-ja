3b190079ab78a09893716a11b65a876e2f3276d2
Adjoa <adjoa.darien@gmail.com>
2018-07-19 04:16:24 +0000

    removed organizer

--- client/src/components/EventForm.js
+++ client/src/components/EventForm.js
@@ -3,12 +3,7 @@ import React from 'react'
 class EventForm extends React.Component {
   constructor() {
     super();
-    
-    this.state = {
-      organizer: {
-        name: '',
-        email: '',
-        phone: '',
+    let initialState = {
         event: {
           name: '',
           start_time: '',
@@ -23,65 +18,63 @@ class EventForm extends React.Component {
           description: ''
         }
       }
-    }
+    
+    this.state = initialState;
   }
   
   handleChange = (event) => {
+    console.log(event.target)
     const { value, name } = event.target;
     this.setState({ [name]: value });
+    console.log(this.state)
   }
   
   handleSubmit = (event) => {
     event.preventDefault();
+    this.props.store.dispatch({
+      type: 'ADD_EVENT',
+      organizer: this.state,
+    });
+    this.setState(this.initialState)
   }
   
   render() {
     return(
       <div>
         <form onSubmit={this.handleSubmit} >
-          <h2>Organizer Info</h2>
-            <label>Name</label>
-            <input type="text" name="organizer[name]"
-              onChange={this.handleChange} />
-            <label>Email</label>
-            <input type="email" name="organizer[email]"
-              onChange={this.handleChange} />
-            <label>Phone</label>
-            <input type="tel" name="organizer[phone]"
-              onChange={this.handleChange} />
           <h2>Event Info</h2>
             <label>Name</label>
             <input type="text" 
-              name="organizer[event][name]"
+              name="event[name]"
               onChange={this.handleChange} />
             <label>Start</label>
             <input type="datetime-local" 
-              name="organizer[event][start_time]"
+              name="event[start_time]"
               onChange={this.handleChange} />
             <label>End</label>
             <input type="datetime-local" 
-              name="organizer[event][end_time]"
+              name="event[end_time]"
               onChange={this.handleChange} />
             <h3>Address</h3>
               <label>Line 1</label>
               <input type="text" 
-                name="organizer[event][address][line1]"
+                name="event[address][line1]"
                 onChange={this.handleChange} />
               <label>Line 2</label>
               <input type="text" 
-                name="organizer[event][address][line2]"
+                name="event[address][line2]"
                 onChange={this.handleChange} />
               <label>City</label>
               <input type="text" 
-                name="organizer[event][address][city]"
+                name="event[address][city]"
                 onChange={this.handleChange} />
               <label>Parish</label>
               <input type="text" 
-                name="organizer[event][address][parish]"
+                name="event[address][parish]"
                 onChange={this.handleChange} />
             <h3>Description</h3>
             <input type="textarea"
-              name="organizer[event][description]"
+              name="event[description]"
               onChange={this.handleChange} />
           <input type="submit" value="Submit"/>
         </form>
