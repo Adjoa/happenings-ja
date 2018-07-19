db353c9fecdcb3364a8a1770817c6dd6d163d1e5
Adjoa <adjoa.darien@gmail.com>
2018-07-19 04:48:55 +0000

    cleaned up routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,6 @@ Rails.application.routes.draw do
   
   # namespace the controllers without affecting the URI
   scope module: :v1, constraints: ApiVersion.new('v1', true) do
-    resources :events, only: [:index, :show]
-    resources :organizers, except: [:new, :edit] do
-      resources :events, except: [:index, :show, :new, :edit]
-    end
+    resources :events
   end
-  
-  post 'auth/login', to: 'authentication#authenticate'
-  post 'signup', to: 'organizers#create'
-  
 end
