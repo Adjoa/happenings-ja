85b201e47caadd63d9e761c9dbacee49c407adb4
Adjoa <adjoa.darien@gmail.com>
2018-07-19 02:47:47 +0000

    updated GET_EVENTS reducer with Object.assign for deep merging

--- client/src/reducers/events_reducer.js
+++ client/src/reducers/events_reducer.js
@@ -4,7 +4,8 @@ export default function eventsReducer(state = { loading: false, events: [] }, ac
       return Object.assign({}, state, {loading: true})
     case 'GET_EVENTS':
       console.log("Getting events...")
-      return { ...state, events: action.events }
+      // return { ...state, events: action.events }
+      return Object.assign({}, state, {loading: false, events: action.events})
  
     default:
       return state;
