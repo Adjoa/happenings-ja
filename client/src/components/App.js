25467c239ab14c3c06c81e3efba734522498880c
Adjoa <adjoa.darien@gmail.com>
2018-07-19 13:57:58 +0000

    removed eventsReady logic from App.js

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -9,27 +9,17 @@ import NotFound from './NotFound';
 import Footer from './Footer';
 
 class App extends React.Component {
-  componentDidMount() {
-    this.props.getEvents()
-  }
-  
   render() {
-    let eventsReady = !this.props.loading;
-    
     return (
       <div>
-      { eventsReady ?
-        <div>
-          <Header/>
-          <NavBar/>
-          <Switch>
-            <Route exact path="/" component={Welcome} />
-            <Route path="/events" component={EventsPage} />
-            <Route component={NotFound} />
-          </Switch>
-          <Footer/>
-        </div> : "Loading..."
-      }
+        <Header/>
+        <NavBar/>
+        <Switch>
+          <Route exact path="/" component={Welcome} />
+          <Route path="/events" component={EventsPage} />
+          <Route component={NotFound} />
+        </Switch>
+        <Footer/>
       </div>
     );
   }
