d817e4d77a09644775b2ca3e0fee842c1d5ec2f3
Adjoa <adjoa.darien@gmail.com>
2018-07-31 17:24:52 +0000

    assessment: added like button via local state

--- client/src/components/EventsListItem.js
+++ client/src/components/EventsListItem.js
@@ -1,20 +1,42 @@
 import React from 'react';
 import { formatDescription } from '../helpers';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import * as actionCreators from '../actions/eventActions';
 
 import Grid from '@material-ui/core/Grid';
 
-const EventsListItem = ({ event }) => {
-  const descriptionAvailable = event.description
+// const EventsListItem = ({ event }) => {
+class EventsListItem extends React.Component {
+  constructor(){
+    super()
+    this.state = {
+      likes: 0
+    }
+  }
   
-  return(
-    <Grid item lg={24}>
-    <div>
-      <span>{ event.start_time }</span>
-      <h2>{ event.name }</h2>
-      <p>{ descriptionAvailable ? formatDescription(event.description) : "No description provided." }</p>
-    </div>
-    </Grid>
-  );
+  handleClick = (event) => {
+    let likesPlusOne = this.state.likes + 1
+    this.setState(
+      {likes: likesPlusOne}
+      )
+  }
+  
+  render() {
+    const descriptionAvailable = this.props.event.description
+    
+    return(
+      <Grid item lg={24}>
+      <div>
+        <span>{ this.props.event.start_time }</span>
+        <h2>{ this.props.event.name }</h2>
+        <p>{ descriptionAvailable ? formatDescription(this.props.event.description) : "No description provided." }</p>
+        <span>Likes: {this.state.likes}</span>
+        <button onClick={this.handleClick}>Like</button>
+      </div>
+      </Grid>
+    );
+  }
 };
   
 export default EventsListItem;
