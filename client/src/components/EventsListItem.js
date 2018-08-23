f138e70d52f951b16447ea25bd0dae0aa22e21df
Adjoa <adjoa.darien@gmail.com>
2018-08-23 18:40:21 +0000

    removed likes function from EventsListItem

--- client/src/components/EventsListItem.js
+++ client/src/components/EventsListItem.js
@@ -1,27 +1,9 @@
 import React from 'react';
 import { formatDescription } from '../helpers';
-import { bindActionCreators } from 'redux';
-import { connect } from 'react-redux';
-import * as actionCreators from '../actions/eventActions';
 
 import Grid from '@material-ui/core/Grid';
 
-// const EventsListItem = ({ event }) => {
 class EventsListItem extends React.Component {
-  constructor(){
-    super()
-    this.state = {
-      likes: 0
-    }
-  }
-  
-  handleClick = (event) => {
-    let likesPlusOne = this.state.likes + 1
-    this.setState(
-      {likes: likesPlusOne}
-      )
-  }
-  
   render() {
     const descriptionAvailable = this.props.event.description
     
@@ -31,8 +13,6 @@ class EventsListItem extends React.Component {
         <span>{ this.props.event.start_time }</span>
         <h2>{ this.props.event.name }</h2>
         <p>{ descriptionAvailable ? formatDescription(this.props.event.description) : "No description provided." }</p>
-        <span>Likes: {this.state.likes}</span>
-        <button onClick={this.handleClick}>Like</button>
       </div>
       </Grid>
     );
