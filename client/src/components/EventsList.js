0b7b9e3d07f3360b20e61f259afd8a64aa081382
Adjoa <adjoa.darien@gmail.com>
2018-08-24 20:49:19 +0000

    EventsList components uses a MUI Grid component to organise event list items instead of a <ul>

--- client/src/components/EventsList.js
+++ client/src/components/EventsList.js
@@ -1,27 +1,50 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import EventsListItem from './EventsListItem';
-
+import classNames from 'classnames';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import Grid from '@material-ui/core/Grid';
+import { withStyles } from '@material-ui/core/styles';
 
-const EventsList = ({ events }) => {
-  const renderEvents = events.map(event => 
-    <Link key={event.id} to={`/events/${event.id}`}>
-      <EventsListItem event={event} />
-    </Link>
-  );
+const styles = theme => ({
+  layout: {
+    width: 'auto',
+    marginLeft: theme.spacing.unit * 3,
+    marginRight: theme.spacing.unit * 3,
+    [theme.breakpoints.up(1100 + theme.spacing.unit * 3 * 2)]: {
+      width: 1100,
+      marginLeft: 'auto',
+      marginRight: 'auto',
+    },
+  },
+  cardGrid: {
+    padding: `${theme.spacing.unit * 8}px 0`,
+  },
+});
+
+class EventsList extends React.Component {
+  render() {
+    const { events, classes } = this.props;
+    
+    const renderEvents = events.map(event =>
+      <Grid item key={event} sm={6} md={4} lg={3}>
+        <EventsListItem event={event} />
+      </Grid>
+    );
   
-  return(
-    <div>
-      <div>
-       <Grid container spacing={16}>
-        <ul>
-          { renderEvents }
-        </ul>
-        </Grid>
-      </div>
-    </div>
-  );
+    return(
+      <React.Fragment>
+        <CssBaseline />
+        <main>
+          <div className={classNames(classes.layout, classes.cardGrid)}>
+            <Grid container spacing={40}>
+              { renderEvents }
+            </Grid>
+          </div>
+        </main>
+      </React.Fragment>
+    );
+  }
 }
   
-export default EventsList;
+// export default EventsList;
+export default withStyles(styles)(EventsList);
