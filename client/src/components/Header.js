198b6144ded72365cd3cce860091ead74af2d643
Adjoa <adjoa.darien@gmail.com>
2018-08-24 21:08:57 +0000

    styled Header component using MUI AppBar

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,10 +1,42 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import classNames from 'classnames';
 
-const Header = () => (
-  <header>
-    <Link to="/"><h1>Happenings JA</h1></Link>
-  </header>
-  );
+import AppBar from '@material-ui/core/AppBar';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import { withStyles } from '@material-ui/core/styles';
+
+
+const styles = theme => ({
+  appBar: {
+    position: 'relative',
+  },
+});
+
+class Header extends React.Component {
+  render() {
+    const { classes } = this.props;
+    
+    return (
+      <React.Fragment>
+        <AppBar position="static" className={classes.appBar}>
+          <Toolbar>
+            <Typography variant="title" color="inherit" noWrap>
+              Happenings JA
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </React.Fragment>
+      );
+  }
+}
+// const Header = () => (
+//   <header>
+//     <Link to="/"><h1>Happenings JA</h1></Link>
+//   </header>
+//   );
+
   
-export default Header;
+export default  withStyles(styles)(Header);
