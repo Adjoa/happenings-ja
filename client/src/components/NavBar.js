bcea684155b427a76459f769ae76e6963247b9d0
Adjoa <adjoa.darien@gmail.com>
2018-08-24 21:17:35 +0000

    styled NavBar with heroUnit styles

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,23 +1,49 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
-import AppBar from '@material-ui/core/AppBar'
-import Toolbar from '@material-ui/core/Toolbar'
-import Typography from '@material-ui/core/Typography'
+import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
+import { withStyles } from '@material-ui/core/styles';
 
-const NavBar = () => {
-  return (
-    <div>
-    <AppBar position="static">
-      <Toolbar>
-        <Typography variant="title" color="inherit">
-          <NavLink to="/events">Find an event</NavLink>&nbsp;
-          <NavLink to="/events/new">Submit an event</NavLink>
-        </Typography>
-      </Toolbar>
-    </AppBar>
-    </div>
-  );
-};
+const styles = theme => ({
+  heroUnit: {
+    backgroundColor: theme.palette.background.paper,
+  },
+  heroContent: {
+    maxWidth: 600,
+    margin: '0 auto',
+    padding: `${theme.spacing.unit * 8}px 0 ${theme.spacing.unit * 6}px`,
+  },
+  heroButtons: {
+    marginTop: theme.spacing.unit * 4,
+  },
+});
 
-export default NavBar;
+class NavBar extends React.Component {
+  render() {
+    const { classes } = this.props;
+    
+    return(
+      <div className={classes.heroUnit}>
+          <div className={classes.heroContent}>
+            <div className={classes.heroButtons}>
+              <Grid container spacing={16} justify="center">
+                <Grid item>
+                  <Button variant="contained" color="primary" component={Link} to="/events">
+                    Find an event
+                  </Button>
+                </Grid>
+                <Grid item>
+                  <Button variant="outlined" color="primary" component={Link} to="/events/new">
+                    Submit an event
+                  </Button>
+                </Grid>
+              </Grid>
+            </div>
+          </div>
+        </div>
+      );
+  }
+}
+
+export default withStyles(styles)(NavBar);
