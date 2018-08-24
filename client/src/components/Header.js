1722fac27de68c9c98ea67ed23c0c108e5dceda8
Adjoa <adjoa.darien@gmail.com>
2018-08-24 21:43:43 +0000

    removed unused imports from Header.js

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,9 +1,6 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import classNames from 'classnames';
 
 import AppBar from '@material-ui/core/AppBar';
-import CssBaseline from '@material-ui/core/CssBaseline';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import { withStyles } from '@material-ui/core/styles';
