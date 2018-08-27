df744e20ba43e3adda481fc7f234313f5c63af80
Adjoa <adjoa.darien@gmail.com>
2018-08-27 17:54:01 +0000

    added CssBaseline to App component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-
 import Header from './Header';
 import NavBar from './NavBar';
 import NotFound from './NotFound';
 import Footer from './Footer';
-
 import EventsPage from '../containers/EventsPage';
+import CssBaseline from '@material-ui/core/CssBaseline';
 
 class App extends React.Component {
   render() {
     return (
         <div>
+          <CssBaseline />
           <Header/>
           <NavBar/>
           <Switch>
