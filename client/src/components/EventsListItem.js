12d305bbfd7b9a8496bcbbf795852cb1ea99ad73
Adjoa <adjoa.darien@gmail.com>
2018-09-03 04:16:19 +0000

    implemented modal for event details in EventListItem

--- client/src/components/EventsListItem.js
+++ client/src/components/EventsListItem.js
@@ -5,9 +5,24 @@ import Button from '@material-ui/core/Button';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
+import Modal from '@material-ui/core/Modal';
 import Typography from '@material-ui/core/Typography';
 import { withStyles } from '@material-ui/core/styles';
 
+function rand() {
+  return Math.round(Math.random() * 20) - 10;
+}
+
+function getModalStyle() {
+  const top = 50 + rand();
+  const left = 50 + rand();
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
 
 const styles = theme => ({
   card: {
@@ -21,30 +36,60 @@ const styles = theme => ({
   cardContent: {
     flexGrow: 1,
   },
+  paper: {
+    position: 'absolute',
+    width: theme.spacing.unit * 50,
+    backgroundColor: theme.palette.background.paper,
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing.unit * 4,
+  },
 });
 
 class EventsListItem extends React.Component {
+  state = {
+    open: false,
+  };
+
+  handleOpen = () => {
+    this.setState({ open: true });
+  };
+
+  handleClose = () => {
+    this.setState({ open: false });
+  };
+  
   render() {
     const descriptionAvailable = this.props.event.description;
-    const { classes } = this.props;
+    const { classes, event } = this.props;
     
     return (
         <Card className={classes.card}>
           <CardContent className={classes.cardContent}>
             <Typography gutterBottom variant="headline" component="h2">
-              { this.props.event.name }
+              { event.name }
             </Typography>
             <Typography>
-              { this.props.event.start_time }
+              { event.start_time }
             </Typography>
             <Typography>
-               { descriptionAvailable ? formatDescription(this.props.event.description) : "No description provided." }
+               { descriptionAvailable ? formatDescription(event.description) : "No description provided." }
             </Typography>
           </CardContent>
           <CardActions>
-            <Button size="small" color="primary" component={Link} to={`/events/${this.props.event.id}`}>
-              Details
-            </Button>
+            <Button onClick={this.handleOpen} size="small" color="primary">Details</Button>
+            <Modal
+                aria-labelledby="simple-modal-title"
+                aria-describedby="simple-modal-description"
+                open={this.state.open}
+                onClose={this.handleClose}
+              >
+              <div style={getModalStyle()} className={classes.paper}>
+                <Typography>{ event.start_time }</Typography>
+                <Typography>{ event.end_time }</Typography>
+                <Typography>{ event.name }</Typography>
+                <Typography>{ descriptionAvailable ? event.description : "No description provided." }</Typography>
+              </div>
+            </Modal>
           </CardActions>
         </Card>
     );
