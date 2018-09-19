2353f5a918920b92782e2dcc4d0cd1bcd0b77c21
Adjoa <adjoa.darien@gmail.com>
2018-09-19 20:44:04 +0000

    in EventForm, import TextField from material ui library

--- client/src/containers/EventForm.js
+++ client/src/containers/EventForm.js
@@ -8,6 +8,7 @@ import InputLabel from '@material-ui/core/InputLabel';
 import FormHelperText from '@material-ui/core/FormHelperText';
 import FormControl from '@material-ui/core/FormControl';
 import Modal from '@material-ui/core/Modal';
+import TextField from '@material-ui/core/TextField';
 import Typography from '@material-ui/core/Typography';
 import { withStyles } from '@material-ui/core/styles';
  
