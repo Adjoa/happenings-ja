7b0f8cb419eb803b9e9b9f919be6b98db40f94d2
Adjoa <adjoa.darien@gmail.com>
2018-09-19 20:41:49 +0000

    in EventForm convert Start to FormControl

--- client/src/containers/EventForm.js
+++ client/src/containers/EventForm.js
@@ -133,36 +133,45 @@ class EventForm extends React.Component {
               <input type="text" 
                 name="name" placeholder="Name required*"
                 onChange={this.handleChange} /> */}
-                
-              <label>Start</label>
-              <input type="text" 
-                name="start_time" placeholder="Start time required*"
-                onChange={this.handleChange} />
+              
+              <FormControl>
+              <InputLabel>Start</InputLabel>
+              <Input type="text" name="start_time" onChange={this.handleChange}  />
+              <FormHelperText>Required *</FormHelperText>  
+              </FormControl>
+              
               <label>End</label>
               <input type="text" 
                 name="end_time"
                 onChange={this.handleChange} />
+                
               <h3>Address</h3>
+              
                 <label>Line 1</label>
                 <input type="text" 
                   name="line1" placeholder="Line 1 required*"
                   onChange={this.handleAddressChange} />
+                  
                 <label>Line 2</label>
                 <input type="text" 
                   name="line2"
                   onChange={this.handleAddressChange} />
+                  
                 <label>City</label>
                 <input type="text" 
                   name="city" placeholder="City required*"
                   onChange={this.handleAddressChange} />
+                  
                 <label>Parish</label>
                 <input type="text" 
                   name="parish" placeholder="Parish required*"
                   onChange={this.handleAddressChange} />
+                  
               <h3>Description</h3>
               <input type="textarea"
                 name="description"
                 onChange={this.handleChange} />
+                
             <input type="submit" value="Submit"/>
           </form>
           </div>
