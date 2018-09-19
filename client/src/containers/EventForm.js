e22ac0fc50e291b169080bdc06b5a9e45fa2b7cd
Adjoa <adjoa.darien@gmail.com>
2018-09-19 19:45:08 +0000

    in EventForm, contain form in a Modal

--- client/src/containers/EventForm.js
+++ client/src/containers/EventForm.js
@@ -103,43 +103,50 @@ class EventForm extends React.Component {
         <Button onClick={this.handleOpen} size="small" color="primary">
           Submit an event
         </Button>
-        <form onSubmit={this.handleSubmit} >
-          <h2>Event Info</h2>
-            <label>Name</label>
-            <input type="text" 
-              name="name" placeholder="Name required*"
-              onChange={this.handleChange} />
-            <label>Start</label>
-            <input type="text" 
-              name="start_time" placeholder="Start time required*"
-              onChange={this.handleChange} />
-            <label>End</label>
-            <input type="text" 
-              name="end_time"
-              onChange={this.handleChange} />
-            <h3>Address</h3>
-              <label>Line 1</label>
+        <Modal
+          aria-labelledby="simple-modal-title"
+          aria-describedby="simple-modal-description"
+          open={this.state.open}
+          onClose={this.handleClose}
+        >
+          <form onSubmit={this.handleSubmit} >
+            <h2>Event Info</h2>
+              <label>Name</label>
               <input type="text" 
-                name="line1" placeholder="Line 1 required*"
-                onChange={this.handleAddressChange} />
-              <label>Line 2</label>
+                name="name" placeholder="Name required*"
+                onChange={this.handleChange} />
+              <label>Start</label>
               <input type="text" 
-                name="line2"
-                onChange={this.handleAddressChange} />
-              <label>City</label>
+                name="start_time" placeholder="Start time required*"
+                onChange={this.handleChange} />
+              <label>End</label>
               <input type="text" 
-                name="city" placeholder="City required*"
-                onChange={this.handleAddressChange} />
-              <label>Parish</label>
-              <input type="text" 
-                name="parish" placeholder="Parish required*"
-                onChange={this.handleAddressChange} />
-            <h3>Description</h3>
-            <input type="textarea"
-              name="description"
-              onChange={this.handleChange} />
-          <input type="submit" value="Submit"/>
-        </form>
+                name="end_time"
+                onChange={this.handleChange} />
+              <h3>Address</h3>
+                <label>Line 1</label>
+                <input type="text" 
+                  name="line1" placeholder="Line 1 required*"
+                  onChange={this.handleAddressChange} />
+                <label>Line 2</label>
+                <input type="text" 
+                  name="line2"
+                  onChange={this.handleAddressChange} />
+                <label>City</label>
+                <input type="text" 
+                  name="city" placeholder="City required*"
+                  onChange={this.handleAddressChange} />
+                <label>Parish</label>
+                <input type="text" 
+                  name="parish" placeholder="Parish required*"
+                  onChange={this.handleAddressChange} />
+              <h3>Description</h3>
+              <input type="textarea"
+                name="description"
+                onChange={this.handleChange} />
+            <input type="submit" value="Submit"/>
+          </form>
+        </Modal>
       </div>
     );
   }
