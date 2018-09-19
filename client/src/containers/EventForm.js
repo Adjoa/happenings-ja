06ca6c6fbe90d66e098e6e9a1685f89dc75260ff
Adjoa <adjoa.darien@gmail.com>
2018-09-19 19:29:01 +0000

    in EventForm, added styles containing paper style for modal

--- client/src/containers/EventForm.js
+++ client/src/containers/EventForm.js
@@ -21,6 +21,16 @@ function getModalStyle() {
   };
 }
 
+const styles = theme => ({
+  paper: {
+    position: 'absolute',
+    width: theme.spacing.unit * 50,
+    backgroundColor: theme.palette.background.paper,
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing.unit * 4,
+  },
+});
+
 class EventForm extends React.Component {
   constructor() {
     super();
