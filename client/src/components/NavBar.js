7a3a3235a357f1e66b2aa1461f977625492e726a
Adjoa <adjoa.darien@gmail.com>
2018-09-19 19:53:54 +0000

    in NavBar, import EventForm from containers/

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import EventForm from './EventForm';
+import EventForm from '../containers/EventForm';
 import { Link } from 'react-router-dom';
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
