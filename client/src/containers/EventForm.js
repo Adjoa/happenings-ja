b98d81a2a7d8e70b08776bb90891161705699543
Adjoa <adjoa.darien@gmail.com>
2018-09-19 20:19:51 +0000

    in EventForm, added container style to styles object

--- client/src/containers/EventForm.js
+++ client/src/containers/EventForm.js
@@ -27,16 +27,20 @@ function getModalStyle() {
 }
 
 const styles = theme => ({
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  formControl: {
+    margin: theme.spacing.unit,
+  },
   paper: {
     position: 'absolute',
     width: theme.spacing.unit * 50,
     backgroundColor: theme.palette.background.paper,
     boxShadow: theme.shadows[5],
     padding: theme.spacing.unit * 4,
-  },
-  formControl: {
-    margin: theme.spacing.unit,
-  },
+  }
 });
 
 class EventForm extends React.Component {
